8e508c589acdb9c0cd6b0969889c7594e1a988a8
Piyush-t24 <piyushiitian2023@gmail.com>
2025-06-24 19:42:14 +0530

    Opportunities Section

---
+++ src/Page/OpportunitiesHub/commonComponents/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ArrowLeft } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 w-full border-b border-[rgba(20,47,33,0.1)] bg-[#0d3528] text-white shadow-md">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3">
+        <a href="/home">
+          <button className="flex items-center gap-2 rounded-full border border-white p-2 hover:bg-[#092413]">
+            <ArrowLeft className="h-5 w-5" />
+            <span className="hidden md:inline">Back</span>
+          </button>
+        </a>
+
+        <div className="text-2xl font-bold">
+          <img
+            src="./RGVerse ICON.png"
+            alt="DevDisplay"
+            className="h-12 w-12"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
