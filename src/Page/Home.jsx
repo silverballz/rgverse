5e0108788a0f96a60e398750f2953c872b3fbdd9
Piyush-t24 <piyushiitian2023@gmail.com>
2025-07-04 16:55:31 +0530

    Changes the position of Div

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -191,20 +191,20 @@ const TechFeatures = () => {
       link: "/ProjectShowcase",
     },
     {
-      title: "Opportunities",
-      description:
-        "Explore various career and learning opportunities in the tech industry.",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8ijqoWoPj5GXXP1WNAyp5C12DzyDGd0t_7zRkH9CBRA&s&ec=72940544",
-      link: "/opportunities",
-    },
-    {
       title: "Resources",
       description:
         "Access a wide range of learning resources, tools, and libraries to boost your skills.",
       image:
         "https://images.unsplash.com/photo-1553877522-43269d4ea984?auto=format&fit=crop&w=2070&q=80",
       link: "/ResourcesHub",
+    },
+    {
+      title: "Opportunities",
+      description:
+        "Explore various career and learning opportunities in the tech industry.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8ijqoWoPj5GXXP1WNAyp5C12DzyDGd0t_7zRkH9CBRA&s&ec=72940544",
+      link: "/opportunities",
     },
     {
       title: "Discussion",
