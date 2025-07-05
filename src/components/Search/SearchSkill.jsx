98a9a85936f6abe81f3df6177bdb51ca1dbb1104
Piyush-t24 <piyushiitian2023@gmail.com>
2025-07-05 17:59:41 +0530

    Search functionality for branch, skills, and location

--- src/components/Search/SearchSkill.jsx
+++ src/components/Search/SearchSkill.jsx
@@ -9,7 +9,7 @@ export default function SearchSkill({ skill, setSearchSkills }) {
   };
 
   return (
-    <div className=" m-2 flex flex-row items-center rounded-md border-2 border-[#00A6FB] bg-[#00A6FB] p-2 text-black transition-colors duration-1000 ease-in-out   hover:bg-white hover:text-[#00A6FB] dark:text-white dark:hover:bg-[#141D2F]">
+    <div className=" m-2 flex flex-row items-center rounded-md border border-[#cffab6] bg-[#163e00] p-2 text-[#c5e3bf] transition-colors duration-1000 ease-in-out   hover:bg-white hover:text-[#cffab6] dark:text-white dark:hover:bg-[#0d3528]">
       <p className="mr-4">{skill}</p>
       <FaTimes
         onClick={() => {
