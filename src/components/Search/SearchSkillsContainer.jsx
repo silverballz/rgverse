07fb995c1eec328bc5e6521b52a34a3283695d92
Piyush-t24 <piyushiitian2023@gmail.com>
2025-07-05 18:00:03 +0530

    Search functionality for branch, skills, and location

--- src/components/Search/SearchSkillsContainer.jsx
+++ src/components/Search/SearchSkillsContainer.jsx
@@ -2,9 +2,15 @@ import SearchSkill from './SearchSkill';
 
 export default function SearchSkillsContainer({ searchSkills, setSearchSkills }) {
   return (
-    <div className="relative mt-2 flex min-h-20 w-full flex-row flex-wrap rounded-lg bg-white p-2 dark:bg-[#1E2A47]">
+    <div className="relative mt-2 flex min-h-20 w-full flex-row flex-wrap rounded-lg bg-white p-2 dark:bg-[#0d3528]">
       {searchSkills.map((skill, idx) => {
-        return <SearchSkill skill={skill} key={idx} setSearchSkills={setSearchSkills} />;
+        return (
+          <SearchSkill
+            skill={skill}
+            key={idx}
+            setSearchSkills={setSearchSkills}
+          />
+        );
       })}
     </div>
   );
